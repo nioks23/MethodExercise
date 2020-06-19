5112bfcb993a96ba5db802629ffc6aa638f7a508
NIOKHOBAYE DIOUF <niokhobayediouf@MacBook-Pro.local>
2020-06-19 14:28:11 -0400

    method exercise 1

---
+++ MethExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello - Waht is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a very popular color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{animal} is an adorable creature! Who is your favorite musician?");
+            var musician = Console.ReadLine();
+
+            Console.WriteLine($"Really !!! I love {musician} songs");
+
+            Console.WriteLine($"Here are your favorite things {userName}");
+
+            Console.WriteLine($"Your favorite color: {color}");
+            Console.WriteLine($"Your favorite animal: {animal}");
+            Console.WriteLine($"Your favorite musician: {musician}");
+
+        }
+    }
+}
